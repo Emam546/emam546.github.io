520d10580dd206b133ef1179a22dd2179a12486f
Emam546 <emam54637@gmail.com>
2023-07-07 17:31:49 +0300

    Initial commit from Create Next App

---
+++ tailwind.config.js
@@ -0,0 +1,33 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+        "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+        "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+        "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    ],
+    theme: {
+        extend: {
+            extend: {
+                colors: {
+                    "primary-light": "#F7F8FC",
+                    "secondary-light": "#FFFFFF",
+                    "ternary-light": "#f6f7f8",
+
+                    "primary-dark": "#0D2438",
+                    "secondary-dark": "#102D44",
+                    "ternary-dark": "#1E3851",
+                },
+                container: {
+                    padding: {
+                        DEFAULT: "1rem",
+                        sm: "2rem",
+                        lg: "5rem",
+                        xl: "6rem",
+                        "2xl": "8rem",
+                    },
+                },
+            },
+        },
+    },
+    plugins: [],
+};
