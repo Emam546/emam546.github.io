10ef9224740ea0fd9e58affc5431769c5240a591
Emam546 <emam54637@gmail.com>
2023-12-23 23:02:34 +0200

    remove unused elements

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -30,22 +30,22 @@ const About: NextPage<Props> = ({ aboutMe, avatar }) => {
             </motion.div>
 
             {/** Counter without paddings */}
-            <motion.div
+            {/* <motion.div
                 initial={{ opacity: 0 }}
                 animate={{ opacity: 1 }}
                 exit={{ opacity: 0 }}
             >
                 <AboutCounter />
-            </motion.div>
+            </motion.div> */}
 
-            <motion.div
+            {/* <motion.div
                 initial={{ opacity: 0 }}
                 animate={{ opacity: 1 }}
                 exit={{ opacity: 0 }}
                 className="container mx-auto"
             >
                 <AboutClients />
-            </motion.div>
+            </motion.div> */}
         </>
     );
 };
